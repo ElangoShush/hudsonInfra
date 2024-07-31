173a64f7924639ff6ae56ef433fa30375b968af1
ElangoShush <elango@shush.pw>
2024-08-01 03:42:01 +0530

    Add files via upload
    Intial Commit

---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+# terraform.tfvars
+# terraform.tfvars
+# project_id = "sherlock-003"
+# region = "us-central1"
+project_id = "sherlock-003"
+region = "us-central1"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+# variables.tf
+
+variable "project_id" {
+  description = "The GCP project ID"
+  type        = string
+}
+variable "region" {
+  description = "The GCP region"
+  type        = string
+  default     = "us-central1"
+}
